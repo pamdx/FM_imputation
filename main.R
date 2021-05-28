5ed4aba668325b1f9f65259ab3a45332ab9ee55a
Pierre Maudoux <pierre.ami.maudoux@gmail.com>
2021-05-28 19:06:39 +0200

    Added fit thresholds for regression and trend. Added variable to choose consecutive or year-by-year aggregated imputation.

--- main.R
+++ main.R
@@ -21,44 +21,50 @@ theme_set(theme_bw())
 
 source("./modules/functions_shiny.R")
 
-# App parameters
-
-  # Data import
-
-country_input <- "France"
-OC1_input <- "Marine fishing"
-start_year <- 1995
-end_year <- 2018
-
 # Data import
 
 source("./modules/data_import_shiny.R")
+
+# Main filtering
+
+country_input <- "Thailand"
+OC2_input <- "Marine fishing"
+start_year <- 1995
+end_year <- 2019
 
 # Subseries-related analyses
 
 source("./modules/subseries_analysis.R")
 
-# App parameters
+#### Estimations parameters ####
 
   # Reg
 
-share_valid_reg <- 0.3
-obs_threshold_linreg <- round(length(years_all)*share_valid_reg)
-reg_type <- 1
+share_valid_reg <- 0.1
+obs_threshold_linreg <- round(length(years_all) * share_valid_reg)
+reg_type <- 1 # 1 = automatic, 2 = manual
 trend <- seq(start_year:end_year)
-reg_dynamic <- emp_value ~ trend + prod_value + labor_value + fleet_value # Choose independent variables from: trend, prod_value, labor_value, fleet_value (separated by "+")
+reg_dynamic <- emp_value ~ trend + prod_value + labor_value # Choose independent variables from: trend, prod_value, labor_value, fleet_value (separated by "+")
+fit_threshold_reg <- 0.85
 
   # Trend
 
-share_valid_trend <- 0.3
-obs_threshold_trend <- round(length(years_all)*share_valid_trend)
+share_valid_trend <- 0.1
+obs_threshold_trend <- round(length(years_all) * share_valid_trend)
+fit_threshold_trend <- 0.85
 
   # LBFH
 
 histavg_threshold <- 5 # Number of previous years on which to base estimates, make as interactive input in Shiny App
 histgrowth_threshold <- 5
 
-  # Aggregated imputation
+#####
+
+# Aggregated imputation
+
+source("./modules/processing_aggregated.R")
+
+agg_imputation_type <- 1 # 1 = "Consecutive", 2 = "Year-by-year"
 
 source("./modules/imputation_aggregated.R")
 
