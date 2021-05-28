228a7ad0702ce170dbe43fb0f071c2ed27785d84
Pierre Maudoux <pierre.ami.maudoux@gmail.com>
2021-05-28 15:11:34 +0200

    Now also prints a visualization of current estimates.

--- modules/final_data_export_viz.R
+++ modules/final_data_export_viz.R
@@ -1,31 +1,33 @@
-# Clean imputed data and assign time stamp
+# Save current estimates chart
 
-imputed_data <- imputed_data_final(imputeddata = imputed_data)
+data_viz(data = FM_exisiting_estimates, countryinput = country_input, OC2input = OC2_input, title = "Visualization of current estimates")
+
+ggsave(paste0("./outputs/", country_input, "_", OC2_input, "_existing.png"), dpi = 300)
 
 # Generate chart of imputed data
 
-data_viz(data = imputed_data, countryinput = country_input, OC1input = OC1_input, title = "Visualization of final imputed data")
+data_viz(data = imputed_data, countryinput = country_input, OC2input = OC2_input, title = "Visualization of final imputed data")
 
-ggsave(paste0("./outputs/", country_input, "_", OC1_input, "_imputed.png"), dpi = 300)
+ggsave(paste0("./outputs/", country_input, "_", OC2_input, "_imputed.png"), dpi = 300)
 
 # Save imputed data as CSV
 
-write_excel_csv(imputed_data, paste0("./outputs/", country_input, "_", OC1_input, "_imputed.csv"), na = "")
+write_excel_csv(imputed_data, paste0("./outputs/", country_input, "_", OC2_input, "_imputed.csv"), na = "")
 
 # Create comparison table between original data and processed data
 
 ctable = compare_df(imputed_data %>%
                       select(geographic_area:comment) %>%
-                      arrange(geographic_area, year, OC1, OC2, working_time, sex), 
+                      arrange(geographic_area, year, OC2, OC3, working_time, sex), 
                     FM_exisiting_estimates %>% 
-                      select(geographic_area, OC1, OC2, working_time, sex, year, value, flag, comment) %>%
-                      arrange(geographic_area, year, OC1, OC2, working_time, sex), 
-                    c("geographic_area", "OC1", "OC2", "working_time", "sex", "year"))
+                      select(geographic_area, OC2, OC3, working_time, sex, year, value, flag, comment) %>%
+                      arrange(geographic_area, year, OC2, OC3, working_time, sex), 
+                    c("geographic_area", "OC2", "OC3", "working_time", "sex", "year"))
 
 # Output HTML table
 
 create_output_table(
   ctable,
   output_type = "html",
-  file_name = paste0("./outputs/", country_input, "_", OC1_input, "_comparison.html"),
+  file_name = paste0("./outputs/", country_input, "_", OC2_input, "_comparison.html"),
   limit = 1000)
