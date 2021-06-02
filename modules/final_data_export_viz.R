544c3d5e43c7b62133ecae08bd055d94caf4cb60
Pierre Maudoux <pierre.ami.maudoux@gmail.com>
2021-06-02 18:11:58 +0200

    Replaced exports of existing estimates chart/imputed data chart/comparison table with the generation of the R Markdown report.

--- modules/final_data_export_viz.R
+++ modules/final_data_export_viz.R
@@ -1,33 +1,13 @@
-# Save current estimates chart
+# Calculate productivity with imputed data
 
-data_viz(data = FM_exisiting_estimates, countryinput = country_input, OC2input = OC2_input, title = "Visualization of current estimates")
+productivity_imputed <- productivity_table(imputed_data %>%
+                                             group_by(year) %>%
+                                             summarise(emp_value = sum(value)), prod_agg)
 
-ggsave(paste0("./outputs/", country_input, "_", OC2_input, "_existing.png"), dpi = 300)
+# Create R Markdown report
 
-# Generate chart of imputed data
-
-data_viz(data = imputed_data, countryinput = country_input, OC2input = OC2_input, title = "Visualization of final imputed data")
-
-ggsave(paste0("./outputs/", country_input, "_", OC2_input, "_imputed.png"), dpi = 300)
+rmarkdown::render("report.Rmd", output_file = paste0("./outputs/", country_input, "_", OC2_input, "_report.html"))
 
 # Save imputed data as CSV
 
 write_excel_csv(imputed_data, paste0("./outputs/", country_input, "_", OC2_input, "_imputed.csv"), na = "")
-
-# Create comparison table between original data and processed data
-
-ctable = compare_df(imputed_data %>%
-                      select(geographic_area:comment) %>%
-                      arrange(geographic_area, year, OC2, OC3, working_time, sex), 
-                    FM_exisiting_estimates %>% 
-                      select(geographic_area, OC2, OC3, working_time, sex, year, value, flag, comment) %>%
-                      arrange(geographic_area, year, OC2, OC3, working_time, sex), 
-                    c("geographic_area", "OC2", "OC3", "working_time", "sex", "year"))
-
-# Output HTML table
-
-create_output_table(
-  ctable,
-  output_type = "html",
-  file_name = paste0("./outputs/", country_input, "_", OC2_input, "_comparison.html"),
-  limit = 1000)
