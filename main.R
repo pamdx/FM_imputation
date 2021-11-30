04e76465a1588a63636616d41dadfb8b2a41cfe7
Pierre Maudoux <pierre.ami.maudoux@gmail.com>
2021-11-30 19:45:50 +0100

    Improved comments to guide user.

--- main.R
+++ main.R
@@ -1,6 +1,6 @@
 ### EMPutator beta ###
 
-rm(list=ls())
+rm(list=ls()) # clear R environment
 
 ### Load packages
 
@@ -13,9 +13,9 @@ library(compareDF)
 library(stargazer)
 library(gridExtra)
 
-theme_set(theme_bw())
+theme_set(theme_bw()) # set simple theme for charts
 
-# Run functions
+# Load app functions
 
 source("./modules/functions.R")
 
@@ -25,10 +25,10 @@ source("./modules/data_import.R")
 
 # Main filtering
 
-country_input <- "Chile"
+country_input <- "France"
 OC2_input <- "Marine fishing"
 start_year <- 1995
-end_year <- 2019
+end_year <- 2020
 
 # Subseries-related analyses
 
@@ -36,25 +36,25 @@ source("./modules/subseries_analysis.R")
 
 #### Estimations parameters ####
 
-  # Reg
+  # Linear regression
 
-share_valid_reg <- 0
-obs_threshold_linreg <- round(length(years_all) * share_valid_reg)
-reg_type <- 1 # 1 = automatic, 2 = manual
-trend <- seq(start_year:end_year)
-reg_dynamic <- emp_value ~ prod_value + labor_value # Choose independent variables from: trend, prod_value, labor_value, fleet_value (separated by "+")
-fit_threshold_reg <- 0.1
+share_valid_reg <- 0.5 # Proportion of years with official data necessary to run regression (to avoid generating estimates from too little information)
+obs_threshold_linreg <- round(length(years_all) * share_valid_reg) # Do not modify
+reg_type <- 1 # Regression type 1 = automatic (runs predetermined models and selects the one with best fit), 2 = manual (see below)
+trend <- seq(start_year:end_year) # Do not modify
+reg_dynamic <- emp_value ~ prod_value + labor_value # Specify manual regression by choosing independent variables from: trend, prod_value, labor_value, fleet_value (separated by "+")
+fit_threshold_reg <- 0.8 # R2-squared threshold for the regression to be taken in consideration
 
-  # Trend
+  # Polynomial trends
 
-share_valid_trend <- 0.5
-obs_threshold_trend <- round(length(years_all) * share_valid_trend)
-fit_threshold_trend <- 0.1
+share_valid_trend <- 0.5 # Proportion of years with official data necessary to run regression (to avoid generating estimates from too little information)
+obs_threshold_trend <- round(length(years_all) * share_valid_trend) # Do not modify
+fit_threshold_trend <- 0.8 # R2-squared threshold for the regression to be taken in consideration
 
-  # Historical growth/avg
+  # Historical growth/average
 
-histavg_threshold <- 5 # Number of previous years on which to base estimates, make as interactive input in Shiny App
-histgrowth_threshold <- 5
+histavg_threshold <- 5 # Number of previous years on which to base historical average estimates
+histgrowth_threshold <- 5 # Number of previous years on which to base historical growth  estimates
 
 #####
 
