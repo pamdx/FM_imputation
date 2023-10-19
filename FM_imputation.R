1f480e0cad63f35910bb24e263a5e775cb360f4b
Pierre Maudoux <pierre.ami.maudoux@gmail.com>
2023-10-19 17:27:47 +0200

    - Renamed scripts to conform to a more coherent naming convention across FM apps - Created "parameters" folders

--- FM_imputation.R
+++ FM_imputation.R
@@ -9,16 +9,23 @@ output_directory <- "./outputs/"
 
 path_data_import <- "./modules/data_import.R"
 path_final_data_export_viz <- "./modules/final_data_export_viz.R"
-path_functions <- "./modules/functions.R"
+path_functions <- "./modules/functions_FM_imputation.R"
 path_imputation_aggregated <- "./modules/imputation_aggregated.R"
 path_imputation_subseries <- "./modules/imputation_subseries.R"
 path_initialization <- "./modules/initialization.R"
 path_inputs_update <- "./modules/inputs_update.R"
 path_processing_aggregated <- "./modules/processing_aggregated.R"
-path_report_imputation <- "./modules/report_imputation.Rmd"
+path_report_imputation <- "./modules/report_FM_imputation.Rmd"
 path_subseries_analysis <- "./modules/subseries_analysis.R"
-path_parameters_estimations <- "./modules/parameters_estimations.R"
-path_parameters_emputator <- "./modules/parameters_emputator.R"
+
+path_parameters_estimations <- "./parameters/parameters_estimations.R"
+path_parameters_emputator <- "./parameters/parameters_FM_imputation.R"
+
+#### ####
+
+#### Update input data (uncomment if needed) ####
+
+# source(path_inputs_update)
 
 #### ####
 
