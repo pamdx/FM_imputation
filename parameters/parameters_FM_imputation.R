2228420ab8db1509d0ba74e4825a9018c171538d
Pierre Maudoux <pierre.ami.maudoux@gmail.com>
2023-10-20 10:48:14 +0200

    Moved flag variables in the parameters script

--- parameters/parameters_FM_imputation.R
+++ parameters/parameters_FM_imputation.R
@@ -6,3 +6,8 @@ country_input <- select_country(FM_raw, country_input)
 OC2_input <- select_sector(FM_raw, country_input)
 start_year <- 1995
 end_year <- 2021 # MAKE SURE TO UPDATE
+
+theme_set(theme_bw()) # set simple theme for charts
+flags_official <- c(NA, "B", "I", "M", "P", "Q", "T", "R")
+flag_FAOestimate <- "E"
+flag_remove <- "X"
